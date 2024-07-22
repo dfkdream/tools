024345af8a5f4a091ad46a1dd278fe1b03689d9b
dfkdream <dfkdream@gmail.com>
2024-07-22 19:46:52 +0900

    Add basic-ssl vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+import basicSSL from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
+	server: {
+		proxy: {}
+	},
 	plugins: [
 		sveltekit(),
-		SvelteKitPWA()
+		SvelteKitPWA(),
+		basicSSL({
+			name: "tools-dfkdream-dev"
+		})
 	]
 });
