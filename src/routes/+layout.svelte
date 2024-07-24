4a1722bd76d6a870cc89ba998d58865a73a80477
dfkdream <dfkdream@gmail.com>
2024-07-24 17:55:59 +0900

    Add SvelteKitPWA vite configurations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,23 @@
 <script>
     import '../app.css';
     import { App } from 'konsta/svelte';
+    import { onMount } from 'svelte';
+    import { pwaInfo } from 'virtual:pwa-info';
+
+    $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
+
+    onMount(async ()=>{
+        if (pwaInfo){
+            const { registerSW } = await import('virtual:pwa-register');
+            registerSW({
+                immediate: true,
+            });
+        }
+    });
 </script>
 
 <svelte:head>
+    {@html webManifestLink}
     <title>Tools</title>
 </svelte:head>
   
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,25 @@ export default defineConfig({
 	},
 	plugins: [
 		sveltekit(),
-		SvelteKitPWA(),
+		SvelteKitPWA({
+			manifest: {
+				name: "Tools",
+				short_name: "Tools",
+				description: "some web based tools",
+				background_color: "rgb(247 247 248)",
+				theme_color: "rgb(247 247 248)",
+				lang: "ko-KR",
+				icons: [
+					{
+						src: "favicon.png",
+						sizes: "256x256",
+						type: "image/png",
+						purpose: "any maskable"
+					}
+				]
+			},
+			registerType: "autoUpdate"
+		}),
 		basicSSL({
 			name: "tools-dfkdream-dev"
 		})
