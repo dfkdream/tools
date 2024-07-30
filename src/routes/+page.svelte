e52165a054fd27e8317b9f890ca8a4a65fc39b23
dfkdream <dfkdream@gmail.com>
2024-07-30 14:21:59 +0900

    Add commit information

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-    import { Navbar, Page, List, ListItem } from 'konsta/svelte';
+    import { Navbar, Page, List, ListItem, BlockFooter } from 'konsta/svelte';
+    import { PUBLIC_GITHUB_SHA } from '$env/static/public';
 </script>
 
 <Page>
@@ -9,4 +10,11 @@
         <ListItem href="/weather" title="날씨" />
         <ListItem href="/tuner" title="튜너" />
     </List>
+
+
+    <BlockFooter class="justify-center text-gray-400 dark:text-gray-600">
+        Commit
+        &nbsp;
+        <a href={`https://github.com/dfkdream/tools/commit/${PUBLIC_GITHUB_SHA}`}>{PUBLIC_GITHUB_SHA.slice(0,7)}</a> 
+    </BlockFooter>
 </Page>
